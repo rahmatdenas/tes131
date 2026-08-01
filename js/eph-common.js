b6f4e284e0d1f8af742deaeb69cc1a98bb1c8859
rahmatdenas <120230537+rahmatdenas@users.noreply.github.com>
2026-08-01 12:33:28 +0700

    Update eph-common.js

--- js/eph-common.js
+++ js/eph-common.js
@@ -186,6 +186,7 @@ document.addEventListener('click', function(e) {
   });
   
   Map.on('popupopen', function(e) { 
+	  if (typeof isGameMode !== 'undefined' && isGameMode === true) return;
     e.popup._sudahDiupdate = false;
     let qid = e.popup._qid;
     if (window.location.hash !== '#' + qid) {
